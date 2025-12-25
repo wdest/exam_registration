08abc4ca79f79fd33bb4142466b99d70fed51941
wdest <qasimzade.1806@gmail.com>
2025-12-25 15:24:23 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Navbar from "@/components/Navbar"; // <--- 1. Navbar-ı import edirik
 
 export default function RootLayout({
   children,
@@ -7,8 +8,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="antialiased">
-        {children}
+      <body className="antialiased bg-gray-50"> {/* Arxa fon rəngi əlavə etdim */}
+        
+        {/* 2. Navbar-ı bura qoyuruq ki, hər səhifədə görünsün */}
+        <Navbar />
+
+        {/* 3. Məzmunu main içinə alırıq */}
+        <main className="min-h-[calc(100vh-64px)]">
+           {children}
+        </main>
+        
       </body>
     </html>
   );
