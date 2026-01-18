971091bc04673354b6118f0f5425935c637e3b42
wdest <qasimzade.1806@gmail.com>
2026-01-18 15:54:51 -0500

    Update admin-check.ts

--- lib/admin-check.ts
+++ lib/admin-check.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 
 export function checkAdminAuth() {
   const cookieStore = cookies();
-  const adminCookie = cookieStore.get("super_admin_access"); // Sənin gizli kukin
+  const adminCookie = cookieStore.get("super_admin_access"); 
 
   // Əgər kuki yoxdursa və ya dəyəri 'true' deyilsə -> YALAN
   return adminCookie?.value === "true";
