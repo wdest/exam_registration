f713140f7149f577a80338b51427cc753df8278a
wdest <qasimzade.1806@gmail.com>
2026-01-20 16:13:59 -0500

    Update route.ts

--- app/api/admin-verify/route.ts
+++ app/api/admin-verify/route.ts
@@ -4,31 +4,31 @@ export async function POST(request: Request) {
   try {
     const body = await request.json();
     const { pin } = body;
-
-    // Vercel-dəki Environment Variable-ı oxuyuruq
     const SECRET_PIN = process.env.ADMIN_PASSWORD;
-
-    if (!SECRET_PIN) {
-      return NextResponse.json({ success: false, message: "Server Error: Parol tapılmadı" }, { status: 500 });
-    }
 
     if (pin === SECRET_PIN) {
       const response = NextResponse.json({ success: true });
 
-      // 🔥 PRODUCTION AYARLARI (Maksimum Təhlükəsizlik)
+      // 🔥 PRODUCTION STANDARTI
       response.cookies.set('super_admin_access', 'v2_secure_hash_99881122_matrix_mode', {
-        httpOnly: true, // JavaScript oxuya bilməz (XSS-dən qoruyur)
-        secure: true,   // 🔒 YALNIZ HTTPS! (Vercel-də mütləq true olmalıdır)
-        sameSite: 'lax',// 'Strict' bəzən redirect zamanı kukini itirir. 'Lax' həm təhlükəsizdir, həm login üçün idealdır.
+        httpOnly: true, 
+        
+        // Bu kod avtomatik başa düşür: Sayt Vercel-dədirsə -> Secure: TRUE
+        // Yox əgər Localhost-dursa -> Secure: FALSE (xəta verməsin deyə)
+        secure: process.env.NODE_ENV === 'production', 
+        
+        // ⚠️ BU ÇOX VACİBDİR: 'Lax' qoy ki, redirect zamanı kuki itməsin!
+        sameSite: 'lax', 
+        
         maxAge: 60 * 60, // 1 saat
         path: '/',
       });
 
       return response;
     } else {
-      return NextResponse.json({ success: false, message: "Yanlış Parol" }, { status: 401 });
+      return NextResponse.json({ success: false }, { status: 401 });
     }
   } catch (e) {
-    return NextResponse.json({ success: false, message: "Sistem xətası" }, { status: 500 });
+    return NextResponse.json({ success: false }, { status: 500 });
   }
 }
